7870c0a8a4cd4c6d8ad46241680feb7516de800f
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-30 15:01:50 +0100

    Updated Changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
 const dashboardRoutes = require("./routes/dashboard");
-
+const adminAuthRoutes = require("./routes/admin.auth");
 
 const app = express();
 app.use(express.json());
@@ -15,10 +15,11 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log("MongoDB error:", err.message));
 
-app.use("/auth", authRoutes);
-app.use("/admin", adminRoutes);
-app.use("/", dashboardRoutes);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/admin", adminAuthRoutes);
+app.use("/api", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
