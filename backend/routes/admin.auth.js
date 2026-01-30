7838fe1785e21e6f3d8d9f68500a4a6ed9162445
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-01-30 15:03:50 +0100

    Updated Changes

--- backend/routes/admin.auth.js
+++ backend/routes/admin.auth.js
@@ -1,1 +1,12 @@
-admin.auth.js
+const express = require("express");
+const router = express.Router();
+
+const {
+    adminRegister,
+    adminLogin
+} = require("../controller/admin.auth.controller");
+
+router.post("/register", adminRegister);
+router.post("/login", adminLogin);
+
+module.exports = router;
