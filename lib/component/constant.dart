4b29f503dfd2a594b09ab412af6e660dc70ac2b8
Oluwasegun Badaru <preeminentdeveloper@gmail.com>
2026-03-25 15:35:57 +0100

    Removed API key from code

--- lib/component/constant.dart
+++ lib/component/constant.dart
@@ -1,3 +1,1 @@
 const apiSecretKey = "";
-// const apiSecretKey =
-//     "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";
