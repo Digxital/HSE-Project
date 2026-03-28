ca06cbb83cb4349811b9130aa20ef049c533419d
jefftech3 <jeffcode303@gmail.com>
2026-03-28 10:23:57 +0000

    minor update

--- src/pages/SupervisorLoginPage.tsx
+++ src/pages/SupervisorLoginPage.tsx
@@ -180,7 +180,7 @@ export const SupervisorLoginPage: React.FC = () => {
                     isLoading={isLoading}
                   >
                     <span className="flex items-center justify-center">
-                      Proceed
+                      Sign In
                       <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                       </svg>
